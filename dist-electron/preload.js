477859eceb664a6f2e720b34ffd8c79666b72ef0
MinedAnemone34 <shun-duffy@outlook.jp>
2024-09-02 21:36:38 +0900

    exclude dist-electron

--- dist-electron/preload.js
+++
@@ -1,1 +0,0 @@
-"use strict";const{contextBridge:r,ipcRenderer:t}=require("electron");r.exposeInMainWorld("api",{storeSet:(e,o)=>t.invoke("store:set",e,o),storeGet:e=>t.invoke("store:get",e),storeDelete:e=>t.invoke("store:delete",e)});
