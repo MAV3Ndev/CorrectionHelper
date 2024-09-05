4e3ede19076577b1a6cc5ca1d09eb3f063af0c1c
MinedAnemone34 <shun-duffy@outlook.jp>
2024-09-05 23:07:30 +0900

    versionスコープの追加

--- electron/preload.ts
+++ electron/preload.ts
@@ -4,4 +4,5 @@ contextBridge.exposeInMainWorld('api', {
     storeSet: (key, value) => ipcRenderer.invoke('store:set', key, value),
     storeGet: (key) => ipcRenderer.invoke('store:get', key),
     storeDelete: (key) => ipcRenderer.invoke('store:delete', key),
+    version: () => ipcRenderer.invoke('version'),
 });
